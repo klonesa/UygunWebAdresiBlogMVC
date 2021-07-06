4f916d7597f3ec8d344d83baf0e8cc84f2c4f1c1
myagiz <myagiz24@gmail.com>
2021-07-07 00:43:26 +0300

    last update working all editors

--- WebUI/Scripts/ckeditor/config.js
+++ WebUI/Scripts/ckeditor/config.js
@@ -1,13 +1,12 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-    // config.uiColor = '#AADC6E';
-
+	// config.uiColor = '#AADC6E';
     config.syntaxhighlight_lang = 'csharp';
     config.syntaxhighlight_hideControls = true;
     config.languages = 'tr';
